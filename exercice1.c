350480b59cdd3ff9854bd956c19602528926f78a
AntonyDLeon <tonydeleon66@outlook.com>
2025-01-21 15:40:40 +0000

    Ajout du projet Hello World en C

---
+++ exercice1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+  void afficherDate(){
+    printf("On est le 21 janvier2025.\n");
+  }
+
+  int main() {
+  void afficherDate(){
+    printf("programm dde test\n");
+    afficherDate();
+  }
+}
