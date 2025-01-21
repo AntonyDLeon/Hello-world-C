68b34ee2ce588823ac4649911330828f7df88c9f
AntonyDLeon <tonydeleon66@outlook.com>
2025-01-21 15:41:33 +0000

    Ajout du projet Hello World en C

---
+++ exercice2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+     char nom[] = "Étudiant";
+     printf("Bonjour, %s !\n", nom);
+     return 0;
+}
---
+++ exercice3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//fonction pricipal
+int main() {
+//déclaration d'une variable
+     char nom[] = "Étudiant";
+//affiche le message suivant avec la vriable prècedente
+     printf("Bonjour, %s !\n", nom);
+//fini le code
+     return 0;
+}
