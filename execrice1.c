18810f2fc4e1c3795d57a70d1fa3e04050238ff5
AntonyDLeon <tonydeleon66@outlook.com>
2025-01-21 10:09:14 -0500

    Update execrice1.c

--- execrice1.c
+++ execrice1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, world!\n");
+    // affiche un message de bienvenue
+    printf("Bienvenue a la programmation C !\n");
+    // affiche de la fin
+    printf("Fin de programmation !\n");
     return 0;
 }
